8d0b753d7c5def0508268f46eb1cb54de9404828
Halvor Johan Steine <halvor-steine@hotmail.com>
2020-01-14 18:18:41 +0100

    merging test

--- test_kode.c
+++ test_kode.c
@@ -3,3 +3,5 @@ this is code, uuuh for loops, recursion, compilation errors!
 This line was added in post
 
 lets see what happens if I make a branch
+
+now that we have made a branch lets merge it into master.
