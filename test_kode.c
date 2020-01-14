4a9c217626a48edd0132d73e8a22d73365958cd9
Halvor Johan Steine <halvor-steine@hotmail.com>
2020-01-14 18:23:02 +0100

    deletion of line test

--- test_kode.c
+++ test_kode.c
@@ -5,3 +5,7 @@ This line was added in post
 lets see what happens if I make a branch
 
 now that we have made a branch lets merge it into master.
+
+how about we delete this part: test
+
+did that work?
